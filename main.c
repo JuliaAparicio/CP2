23d700fb1569e21a96cd2b629e50a8d9a723c3dc
Julia Aparicio <juliaaparicio756@gmail.com>
2025-05-06 18:44:37 -0300

    main.c
    ex 1 giovanna
    ex 2 julia
    ex 3 maria eduarda
    ex 4 nicolle

--- main.c
+++ main.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
+// Função para gerar a sequência de Fibonacci
+void Fibonacci() {
+    int n, i;
+
+    printf("Digite a quantidade de termos da sequência de Fibonacci (1 a 50): ");
+    scanf("%d", &n);
+
+    if (n < 1 || n > 50) {
+        printf("Valor incorreto! Digite um numero entre 1 e 50.\n");
+        return;
+    }
+
+    int fib[50];
+    fib[0] = 0;
+
+    if (n > 1)
+        fib[1] = 1;
+
+    for (i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    printf("Sequência de Fibonacci: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fib[i]);
+    }
+    printf("\n");
+}
+
+// Função para calcular fatorial
 int fatorial(int n) {
     int resultado = 1;
     for (int i = 1; i <= n; i++) {
@@ -8,11 +39,17 @@ int fatorial(int n) {
     return resultado;
 }
 
+// Função auxiliar para gerar vetor de fatoriais
 void fatoriais_com_vetores() {
     int n;
 
     printf("Digite um numero inteiro de 1 a 20: ");
     scanf("%d", &n);
+
+    if (n < 1 || n > 20) {
+        printf("Valor inválido. Digite um número entre 1 e 20.\n");
+        return;
+    }
 
     long long fatoriais[n];
 
@@ -23,7 +60,82 @@ void fatoriais_com_vetores() {
     }
 }
 
+// Função chamada pelo menu
 void Fatoriais() {
     fatoriais_com_vetores();
 }
 
+// Função para verificar se uma palavra é palíndromo
+void verificar_palindromo() {
+    char palavra[101];
+    int tam, palindromo = 1;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra);
+
+    tam = strlen(palavra);
+    for (int i = 0; i < tam / 2; i++) {
+        if (palavra[i] != palavra[tam - 1 - i]) {
+            palindromo = 0;
+            break;
+        }
+    }
+
+    if (palindromo)
+        printf("A palavra é um palíndromo.\n");
+    else
+        printf("A palavra NÃO é um palíndromo.\n");
+}
+
+// Função para verificar substring
+void verificar_substring() {
+    char str1[101], str2[101];
+
+    printf("Digite a primeira string: ");
+    scanf("%s", str1);
+    printf("Digite a segunda string: ");
+    scanf("%s", str2);
+
+    if (strstr(str1, str2) != NULL)
+        printf("A segunda string está contida na primeira.\n");
+    else
+        printf("A segunda string NÃO está contida na primeira.\n");
+}
+
+// Menu principal
+int main() {
+    int opcao;
+
+    do {
+        printf("\n===== MENU DE EXERCÍCIOS =====\n");
+        printf("1 - Sequência de Fibonacci\n");
+        printf("2 - Fatoriais\n");
+        printf("3 - Verificar Palíndromo\n");
+        printf("4 - Verificar Substring\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                Fibonacci();
+                break;
+            case 2:
+                Fatoriais();
+                break;
+            case 3:
+                verificar_palindromo();
+                break;
+            case 4:
+                verificar_substring();
+                break;
+            case 0:
+                printf("Finalizando o programa.\n");
+                break;
+            default:
+                printf("Opção incorreta, tente novamente.\n");
+        }
+    } while (opcao != 0);
+
+    return 0;
+}
